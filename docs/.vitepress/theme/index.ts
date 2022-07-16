f9252299ad8551a31531954c9fec1a3ea42a5429
shoppingzh <xpzheng6@163.com>
2022-07-16 20:39:33 +0800

    auto enable dark mode

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -6,6 +6,10 @@ import '../styles/index.less'
 export default {
   ...Theme,
   enhanceApp({ app }) {
+    // 自动启用暗黑模式
+    if (!localStorage.getItem('vitepress-theme-appearance')) {
+      localStorage.setItem('vitepress-theme-appearance', 'dark')
+    }
     app.use(ui)
     app.use(components)
   }
