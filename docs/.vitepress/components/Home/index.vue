6379a8efb461e6b7fdb545935fb915cda537dc98
shoppingzh <xpzheng6@163.com>
2022-12-15 11:22:09 +0800

    change theme, add logo

--- docs/.vitepress/components/Home/index.vue
+++ docs/.vitepress/components/Home/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div ref="el" class="text-2xl font-semibold leading-loose tracking-wider"></div>
+  <div
+    ref="el"
+    class="text-2xl font-semibold leading-loose tracking-wider transition-all duration-300 ease-in-out"
+    :class="{ 'is-done text-3xl': isDone }"></div>
 </template>
 
 <script setup lang="ts">
@@ -7,6 +10,7 @@ import TypeIt from 'typeit'
 import { onMounted, ref } from 'vue'
 
 const el = ref()
+const isDone = ref(false)
 
 onMounted(() => {
   const instance = new TypeIt(el.value, {
@@ -17,11 +21,24 @@ onMounted(() => {
       '很高兴与你相遇，一同学习知识。',
       '希望你能玩得开心！'
     ],
-    speed: 80,
+    speed: 65,
     // loop: true,
     // loopDelay: 15000,
+    afterComplete: () => {
+      setTimeout(() => {
+        isDone.value =  true
+      }, 500)
+    }
   })
   instance.go()
 })
 
 </script>
+
+<style scoped>
+.is-done {
+  @apply text-transparent;
+  background: linear-gradient(to bottom, theme('colors.b-primary.DEFAULT'), theme('colors.b-success.DEFAULT'));
+  -webkit-background-clip: text;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,27 @@
+const config = require('tailwindcss/defaultConfig');
+const colors = require('tailwindcss/colors');
+
+const baseColors = {
+  blue: ['#E8F3FF', '#BEDAFF', '#94BFFF', '#6AA1FF', '#4080FF', '#165DFF', '#0E42D2', '#072CA6', '#031A79', '#000D4D'],
+  orange: ['#FFF7E8', '#FFE4BA', '#FFCF8B', '#FFB65D', '#FF9A2E', '#FF7D00', '#D25F00', '#A64500', '#792E00', '#4D1B00'],
+  green: ['#E8FFEA', '#AFF0B5', '#7BE188', '#4CD263', '#23C343', '#00B42A', '#009A29', '#008026', '#006622', '#004D1C'],
+  red: ['#FFECE8', '#FDCDC5', '#FBACA3', '#F98981', '#F76560', '#F53F3F', '#CB272D', '#A1151E', '#770813', '#4D000A'],
+  gray: ['#F7F8FA', '#F2F3F5', '#E5E6EB', '#C9CDD4', '#A9AEB8', '#86909C', '#6B7785', '#4E5969', '#272E3B', '#1D2129'],
+};
+
+function createPrimaryColors(type, colors) {
+  if (!colors) return {};
+  return {
+    [`b-${type}`]: {
+      DEFAULT: colors[5],
+      hover: colors[4],
+      active: colors[6],
+      disabled: colors[2],
+      background: colors[0],
+    }
+  };
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,13 +30,32 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
+    colors: {
+      ...colors,
+
+      ...Object.entries(baseColors).reduce((conf, [name, list]) => {
+        conf[name] = list.reduce((map, color, index) => {
+          map[(index + 1) * 100] = color;
+          return map;
+        }, {});
+        return conf;
+      }, {}),
+
+      ...createPrimaryColors('primary', baseColors.blue),
+      ...createPrimaryColors('success', baseColors.green),
+      ...createPrimaryColors('warn', baseColors.orange),
+      ...createPrimaryColors('danger', baseColors.red),
+      ...createPrimaryColors('link', baseColors.blue),
+
+    },
+    // 继承（原有的不会删除）
     extend: {
       textColor: {
-        primary: '#1D2129',
-        regular: '#4E5969',
-        secondary: '#86909C',
-        disabled: '#C9CDD4',
-      }
+        primary: baseColors.gray[9],
+        regular: baseColors.gray[7],
+        secondary: baseColors.gray[5],
+        disabled: baseColors.gray[3],
+      },
     },
   },
   plugins: [],
