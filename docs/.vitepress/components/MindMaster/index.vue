342f7acad7ad1ffc9db0abab3dda1addcda5199e
郑晓平 <xpzheng6@163.com>
2022-12-25 07:16:42 +0000

    update components

---
+++ docs/.vitepress/components/MindMaster/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <iframe
+    v-if="windowWidth != null"
+    :width="windowWidth"
+    height="4000px"
+    class="border-none outline-none"
+    :src="src"></iframe>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const windowWidth = ref()
+
+defineProps({
+  src: String,
+})
+
+onMounted(() => {
+  windowWidth.value = (window.innerWidth || document.documentElement.clientWidth) - 20
+})
+</script>
