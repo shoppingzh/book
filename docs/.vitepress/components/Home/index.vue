7d20d1c6195dc746fdb725f817ff6afec3eb717e
郑晓平 <xpzheng6@163.com>
2022-12-25 07:17:19 +0000

    update

--- docs/.vitepress/components/Home/index.vue
+++ docs/.vitepress/components/Home/index.vue
@@ -3,14 +3,22 @@
     ref="el"
     class="text-2xl font-semibold leading-loose tracking-wider transition-all duration-300 ease-in-out"
     :class="{ 'is-done text-3xl': isDone }"></div>
+  <div v-if="isDone" class="mt-10">
+    <el-button type="primary" class="animate__animated animate__fadeInLeft animate__faster" @click="goFe">
+      <el-icon class="mr-2"><Guide /></el-icon>
+      前端工程师技术栈
+    </el-button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import TypeIt from 'typeit'
 import { onMounted, ref } from 'vue'
+import { useRouter } from 'vitepress'
 
 const el = ref()
 const isDone = ref(false)
+const router = useRouter()
 
 onMounted(() => {
   const instance = new TypeIt(el.value, {
@@ -21,7 +29,7 @@ onMounted(() => {
       '很高兴与你相遇，一同学习知识。',
       '希望你能玩得开心！'
     ],
-    speed: 65,
+    speed: 30,
     // loop: true,
     // loopDelay: 15000,
     afterComplete: () => {
@@ -32,6 +40,10 @@ onMounted(() => {
   })
   instance.go()
 })
+
+function goFe() {
+  router.go('fe')
+}
 
 </script>
 
---
+++ docs/fe.md
@@ -0,0 +1,6 @@
+---
+layout: page
+---
+
+
+<MindMaster src="https://edrawcloudpubliccn.oss-cn-shenzhen.aliyuncs.com/viewer/self/24662951/share/2022-12-25/1671950914/main.svg" />
