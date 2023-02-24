673c6ad319c05ec311c7d128a8128bbd9dea4ddd
Shopping <xpzheng6@163.com>
2023-02-24 21:22:09 +0800

    add total visitor count

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -4,6 +4,12 @@
       <el-backtop>
         <svg-icon name="huojian" class="text-2xl" />
       </el-backtop>
+      <div class="flex items-center fixed left-1 bottom-1 z-[101] opacity-60">
+        <div class="text-xs text-secondary">总访问量：</div>
+        <img
+          class="w-[100px]"
+          src="https://profile-counter.glitch.me/zhengxiaoping/count.svg">
+      </div>
     </template>
   </Layout>
 </template>
