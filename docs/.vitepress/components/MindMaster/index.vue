b62c6de1ec7afe556dd316f7d4b936f68c296289
郑晓平 <xpzheng6@163.com>
2023-05-26 13:33:29 +0000

    update

--- docs/.vitepress/components/MindMaster/index.vue
+++ docs/.vitepress/components/MindMaster/index.vue
@@ -1,35 +1,44 @@
 <template>
   <div class="py-10">
-    <div class="px-6 text-right">
+    <div class="px-6 text-xs">
       <el-link type="primary" :underline="false" @click="handleRefresh">
         <el-icon class="mr-1"><Refresh /></el-icon> 刷新脑图
       </el-link>
+      <el-divider direction="vertical" />
+      <el-link type="primary" :underline="false" @click="handleOpenWindow">
+        <el-icon><Connection /></el-icon> 新窗口打开
+      </el-link>
     </div>
-    <img :src="`${src}?_=${time || ''}`">
+    <div v-loading="loading">
+      <img :src="`${src}?_=${time || ''}`">
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, reactive, ref } from 'vue';
 
-const time = ref()
-const docSize = reactive({
-  width: null,
-  height: null,
-})
-
-defineProps({
+const props = defineProps({
   src: String,
 })
+const time = ref()
+const loading = ref(false)
 
 function handleRefresh() {
+  loading.value = true
   time.value = +new Date()
+  setTimeout(() => {
+    loading.value = false
+  }, 1000)
 }
 
-onMounted(() => {
-  const el = document.querySelector('.VPDoc > .container > .content .content-container')
-  if (!el) return
-  docSize.width = el.clientWidth
-  docSize.height = el.clientHeight
-})
+function handleOpenWindow() {
+  window.open(props.src)
+}
 </script>
+
+<style scoped lang="postcss">
+.el-link {
+  @apply text-xs;
+}
+</style>
